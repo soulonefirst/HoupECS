9cea242b041580777d742ec14c3e6424f6962676
soulonefirst <toorbonoob@gmail.com>
2020-05-17 22:01:14 +0300

    предслияние

--- Assets/Scripts/System/Character/ParameterAnimatorSetter.cs
+++ Assets/Scripts/System/Character/ParameterAnimatorSetter.cs
@@ -36,7 +36,6 @@ public class ParameterAnimatorSetter : SystemBase
                     if (takeDamage.takeDamage)
                     {
                         animators[id.Value].SetBool("TakeDamage", true);
-                        takeDamage.takeDamage = false;
                     } else
                     if (animators[id.Value].GetCurrentAnimatorStateInfo(0).IsTag("TakeDamage"))
                     {
@@ -45,6 +44,7 @@ public class ParameterAnimatorSetter : SystemBase
                     if (!animators[id.Value].GetBool("TakeDamage"))
                     {
                         takeDamage.alreadyTakeDamage = false;
+                        takeDamage.takeDamage = false;
                     }
                 }
 
