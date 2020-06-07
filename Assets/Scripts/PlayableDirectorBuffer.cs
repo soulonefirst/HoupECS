dd91a37e3ee610740de9dc61f5335515195f83f4
soul <andryshka.88@mail.ru>
2020-06-07 21:08:35 +0300

    Плайбл

---
+++ Assets/Scripts/PlayableDirectorBuffer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public static class PlayableDirectorBuffer
+{
+    public static PlayableDirector playableDirector;
+    public static TimelineAsset[] timelines;
+}
---
+++ Assets/Scripts/SetPlayableDirector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class SetPlayableDirector : MonoBehaviour
+{
+    public TimelineAsset[] timelineAssets;
+    private void Start()
+    { 
+        PlayableDirectorBuffer.playableDirector = GetComponent<PlayableDirector>();
+        PlayableDirectorBuffer.timelines = timelineAssets;
+    }
+}
